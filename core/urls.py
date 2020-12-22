17a119a826f581928dd87b4506e6a9d0a6ca9715
Naser AlMuhana <naseralmuhana0@gmail.com>
2020-12-22 03:01:48 +0200

    changed

--- core/urls.py
+++ core/urls.py
@@ -8,5 +8,8 @@ app_name = 'core'
 
 urlpatterns = [
     path('', core_views.IndexPageView.as_view(), name='index'),
+    path('<slug>', core_views.store_type_list, name='store_type_list'),
+    path('<type_slug>/<slug>', core_views.store_products_list, name='store_products_list'),
+    
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
--- core/views.py
+++ core/views.py
@@ -10,6 +10,62 @@ class IndexPageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(IndexPageView, self).get_context_data(**kwargs)
-        context['products'] = core_models.Product.objects.all
-        # context['photos'] = core_models.ProductImage.objects.filter(product__name='red shirt')
+        context.update(needed_everywhere())
         return context
+
+
+def store_type_list(request, slug):
+
+    type_slug = core_models.StoreType.objects.filter(slug__iexact=slug)
+    slug = check_on_slug(type_slug)
+    if slug is None:
+        return HttpResponse('<h1>Post Not Found</h1>')
+    type_stores_list = core_models.Store.objects.filter(
+        storetype=slug).order_by('-create_at')
+
+    context = {
+        'type_stores_list': type_stores_list,
+    }
+    context.update(needed_everywhere())
+    return render(request, 'core/index.html', context)
+
+
+def store_products_list(request, type_slug, slug):
+
+    store_slug = core_models.Store.objects.filter(slug__iexact=slug)
+    slug = check_on_slug(store_slug)
+    if slug is None:
+        return HttpResponse('<h1>Post Not Found</h1>')
+    store_products = core_models.Product.objects.filter(
+        category__store=slug).order_by('-create_at')
+    context = {
+        'store_products': store_products,
+    }
+    context.update(needed_everywhere())
+    return render(request, 'core/products_list.html', context)
+
+
+# function to check if the slug is exist and return none if not.
+def check_on_slug(slug):
+
+    if slug.exists():
+        slug = slug.first()
+        return slug
+    else:
+        return None
+
+
+def needed_everywhere():
+    context = {}
+    all_products = {}
+    all_stores = core_models.Store.objects.all().order_by('-create_at')
+    
+    for store in all_stores:
+        all_products[store.name] = core_models.Product.objects.filter(
+            category__store__name=store.name).order_by('-create_at')[:12]
+    
+    context['all_stores'] = all_stores
+    context['all_products'] = all_products
+    context['store_types'] = core_models.StoreType.objects.all()
+
+    return context
